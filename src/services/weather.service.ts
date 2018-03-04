44d2f83098c75e07c7f4dc8b0d6cb7f43c3d0d58
Dmitry Mesh <mailmesher@gmail.com>
2018-03-05 00:47:44 +0400

    queue start

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -6,9 +6,40 @@ import * as querystring from "querystring";
 @Component()
 export class WeatherService {
   private weatherConfig = config.get("weather");
+
+  requestsQueue: any[] = [
+    /* ? */
+  ];
+  /*requestQueueIteratorInit() {
+    setInterval(()=>{
+      if(requestQueue.length && !queueIsDone){
+         requestQueue.forEach( req =>
+         const res = await axious (req.long.lat);
+         req.resolve(res);
+      } else {
+        queueIsDone = true
+        // берем сканируем очереди каждые 20 мс не оптимально
+      }
+    })
+  }*/
   constructor() {}
 
-  async getWeather(latitude: number, longitude: number) {
+  async getWeatherAsQueue(
+    latitude: number,
+    longitude: number
+  ): Promise<any> {
+    this.requestsQueue.push({ latitude, longitude, id: "id" });
+    return new Promise((resolve, reject) => {
+      /* 
+        this.requestsQueue.push({ latitude, longitude, resolve });
+      })*/
+    });
+  }
+
+  async getWeather(
+    latitude: number,
+    longitude: number
+  ): Promise<any> {
     try {
       const weather = await axios.get(
         this.weatherConfig.basePath +
